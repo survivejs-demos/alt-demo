00c293b1c6f3c784b3f700e9850f991637527612
Juho Vepsalainen <bebraw@gmail.com>
2016-05-24 16:58:42 +0300

    `Notes` - Simplify

--- kanban-app/app/components/Notes.jsx
+++ kanban-app/app/components/Notes.jsx
@@ -11,19 +11,19 @@ export default connect(({}), {laneActions: LaneActions})(
     laneActions
   }) => {
     return (
-      <ul className="notes">{notes.map(note =>
-        <li key={note.id}>
-          <Note className="note" id={note.id}
-            editing={note.editing} onMove={laneActions.move}>
+      <ul className="notes">{notes.map(({id, editing, task}) =>
+        <li key={id}>
+          <Note className="note" id={id}
+            editing={editing} onMove={laneActions.move}>
             <Editable
               className="editable"
-              editing={note.editing}
-              value={note.task}
-              onValueClick={onValueClick.bind(null, note.id)}
-              onEdit={onEdit.bind(null, note.id)} />
+              editing={editing}
+              value={task}
+              onValueClick={onValueClick.bind(null, id)}
+              onEdit={onEdit.bind(null, id)} />
             <button
               className="delete"
-              onClick={onDelete.bind(null, note.id)}>x</button>
+              onClick={onDelete.bind(null, id)}>x</button>
           </Note>
         </li>
       )}</ul>
