b752ca37c34835316b65b4072ddc4dcd055e0fc5
Juho Vepsalainen <bebraw@gmail.com>
2016-05-25 18:45:00 +0300

    `Root` -> `Provider`
    Easier to understand this way.

--- kanban-app/app/index.jsx
+++ kanban-app/app/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Root from './components/Root';
+import Provider from './components/Provider';
 
 ReactDOM.render(
-  <Root><App /></Root>,
+  <Provider><App /></Provider>,
   document.getElementById('app')
 );
