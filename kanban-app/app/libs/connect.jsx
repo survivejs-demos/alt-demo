ff5a01d2a1fccb42c1b8640411df013b91d67d5e
Juho Vepsalainen <bebraw@gmail.com>
2016-05-26 09:46:15 +0300

    connect - Simplify further

--- kanban-app/app/libs/connect.jsx
+++ kanban-app/app/libs/connect.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import connect from 'connect-alt';
 
-const connectAdapter = (Component, state, actions) => {
+const connectAdapter = (Component, actions) => {
   return props => <Component {...Object.assign({}, props, actions)} />
 };
 
@@ -9,8 +9,8 @@ export default (state, actions) => {
   if(typeof state === 'function' ||
     (typeof state === 'object') &&
     Object.keys(state).length) {
-    return target => connect(state)(connectAdapter(target, state, actions));
+    return target => connect(state)(connectAdapter(target, actions));
   }
 
-  return target => connectAdapter(target, state, actions);
+  return target => connectAdapter(target, actions);
 };
