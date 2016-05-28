8db6fb21dd5ca97645443d2efd0e30fe5c0bc87c
Juho Vepsalainen <bebraw@gmail.com>
2016-05-28 16:31:09 +0300

    `Notes` - Drop `connect`
    No need for this here.

--- normalized-app/app/components/Notes.jsx
+++ normalized-app/app/components/Notes.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import connect from '../libs/connect';
 import Editable from './Editable';
 import Note from './Note';
 import LaneActions from '../actions/LaneActions';
 
-export default connect(({}), {
-  LaneActions
-})(
-  ({
-    notes,
-    onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {},
-    LaneActions
-  }) => {
-    return (
-      <ul className="notes">{notes.map(({id, editing, task}) =>
-        <li key={id}>
-          <Note className="note" id={id}
+export default ({
+  notes,
+  onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}
+}) => {
+  return (
+    <ul className="notes">{notes.map(({id, editing, task}) =>
+      <li key={id}>
+        <Note className="note" id={id}
+          editing={editing}
+          onClick={onNoteClick.bind(null, id)}
+          onMove={LaneActions.move}>
+          <Editable
+            className="editable"
             editing={editing}
-            onClick={onNoteClick.bind(null, id)}
-            onMove={LaneActions.move}>
-            <Editable
-              className="editable"
-              editing={editing}
-              value={task}
-              onEdit={onEdit.bind(null, id)} />
-            <button
-              className="delete"
-              onClick={onDelete.bind(null, id)}>x</button>
-          </Note>
-        </li>
-      )}</ul>
-    );
-  }
-);
+            value={task}
+            onEdit={onEdit.bind(null, id)} />
+          <button
+            className="delete"
+            onClick={onDelete.bind(null, id)}>x</button>
+        </Note>
+      </li>
+    )}</ul>
+  );
+}
